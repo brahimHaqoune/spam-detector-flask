6cad22f0bf131de217291644b9bf1062fc272189
brahimHaqoune <you@example.com>
2021-05-22 04:27:14 +0100

    commit

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, render_template, url_for, request
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
-import re
-import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize
 import pickle
@@ -23,34 +17,20 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     df = pd.read_csv("data/Youtube01-Psy.csv")
-    corpus = df['CONTENT']
-
-    #data cleaning
-    corpus_clean = []
-    for txt in corpus:
-        word_tokens = [word.lower() for word in word_tokenize(txt)]
-        clean_words = [word for word in word_tokens if (not word in set(stopwords.words('english')) and word.isalpha())]
-        lemmmatizer = WordNetLemmatizer()
-        clean_words = [lemmmatizer.lemmatize(word.lower()) for word in clean_words]
-        corpus_clean.append(' '.join(clean_words))
-
-    df_data = df[['CONTENT', 'CLASS']]
 
     # Features and Labels
-    df_x = df_data['CONTENT']
-    df_y = df_data.CLASS
+    df_x = df['CONTENT']
+    df_y = df.CLASS
 
     #TFIDF
-    corpus = df_x
     cv = TfidfVectorizer(min_df=1, stop_words='english')
-    X = cv.fit_transform(corpus)
+    X = cv.fit_transform(df_x)
 
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X, df_y, test_size=0.33, random_state=42)
 
     #Logistic Regression
     from sklearn.linear_model import LogisticRegression
-    from sklearn.metrics import accuracy_score
 
     LR_model = LogisticRegression(solver='liblinear')
     LR_model.fit(X_train, y_train)
@@ -58,7 +38,6 @@ def predict():
     # Save Model
     pickle.dump(LR_model, open('model.pkl', 'wb'))
 
-    # ytb_model = open('spam_model.pkl', 'rb')
     clf = pickle.load(open('model.pkl', 'rb'))
     if request.method == 'POST':
         comment = request.form['comment']
